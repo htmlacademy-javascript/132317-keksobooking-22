779fb227074bcc282753c5e050e36bec5b6a9f75
Dmitriy <delovar1@gmail.com>
2021-02-02 16:01:15 +0200

    вставка заклинания

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+// eslint-disable-next-line no-unused-vars
 const getRandomInteger = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// eslint-disable-next-line no-unused-vars
 const getRandomFloat = (min, max, decimalPlaces) => {
   return ((Math.random() * (max - min + 1)) + min).toFixed(decimalPlaces);
 }
